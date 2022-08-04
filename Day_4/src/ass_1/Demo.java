a69e9714ca28505d16b98a959a100c3d98b7cae2
Dhruv Gupta <101388885+Dhruva8878@users.noreply.github.com>
2022-08-04 14:14:06 +0530

    updated

--- Day_4/src/ass_1/Demo.java
+++ Day_4/src/ass_1/Demo.java
@@ -5,19 +5,21 @@ public class Demo {
     public static void main(String[] args) {
 
 
-        Thread cal=new Thread(new Runnable() {
-            @Override
-            public void run() {
-                int sum=1;
-                for(int i=1; i<=10; i++){
-                    sum*=i;
-                }
-                System.out.println(sum);
-            }
-        });
+        Thread cal=new Thread(new Sum());
 
         cal.start();
 
+    }
+}
+
+class Sum implements Runnable{
+    @Override
+    public void run() {
+        int sum=1;
+        for(int i=1; i<=10; i++){
+            sum*=i;
+        }
+        System.out.println(sum);
     }
 }
 
